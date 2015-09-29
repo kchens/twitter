e52f315baaf106d793c9fe7e19915ba0caeea669
Kevin Chen <kevin.chen@kchen-mb13r.corp.creditkarma.com>
2015-09-29 14:26:00 -0700

    Adds setup for Twitter client

---
+++ Twitter/TwitterClient.swift
@@ -0,0 +1,13 @@
+//
+//  TwitterClient.swift
+//  Twitter
+//
+//  Created by Kevin Chen on 9/29/15.
+//  Copyright © 2015 Kevin Chen. All rights reserved.
+//
+
+import Cocoa
+
+class TwitterClient: BDBOAuth1RequestOperationManager {
+
+}
