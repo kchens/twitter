dff26c9f1201f6e4b368ca8c647d409e7e0168a1
Kevin Chen <kevin.chen@kchen-mb13r.corp.creditkarma.com>
2015-09-29 14:44:52 -0700

    Adds static instance

--- Twitter/TwitterClient.swift
+++ Twitter/TwitterClient.swift
@@ -6,8 +6,26 @@
 //  Copyright © 2015 Kevin Chen. All rights reserved.
 //
 
-import Cocoa
+import UIKit
+
+let twitterConsumerKey = "wguyfhMI9OP0y74kfxQupoJy0"
+let twitterConsumerSecret = "3AdRrjCtqRWHS2kJt85pN1Po2LvyCxLUBNgjpND1YVNIhoFKci"
+let twitterBaseURL = NSURL(string: "https://api.twitter.com")
 
 class TwitterClient: BDBOAuth1RequestOperationManager {
-
+    
+//    static let sharedInstance = TwitterClient(baseURL: twitterBaseURL,
+//        consumerKey: twitterConsumerKey,
+//        consumerSecret: twitterConsumerSecret)
+    
+    class var sharedInstance: TwitterClient  {
+        struct Static {
+            static let instance = TwitterClient(baseURL: twitterBaseURL,
+                consumerKey: twitterConsumerKey,
+                consumerSecret: twitterConsumerSecret)
+        }
+        
+        return Static.instance
+    }
+    
 }
