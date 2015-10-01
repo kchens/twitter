c98a9a13bb24ac0863cd7360f3b2e431929b0f25
Kevin Chen <kevin.chen@kchen-mb13r.corp.creditkarma.com>
2015-10-01 01:37:17 -0700

    Adds lot of changes, can't setWithUrl

--- Podfile
+++ Podfile
@@ -2,6 +2,11 @@
 # platform :ios, '6.0'
 
 platform :ios, "8.0"
-pod "AFNetworking"
-pod "BDBOAuth1Manager"
 
+use_frameworks! 
+
+target 'Twitter' do
+  pod 'AFNetworking', '~> 2.6.0'
+  pod 'MBProgressHUD', '~> 0.9.1'
+  pod "BDBOAuth1Manager"
+end
--- Twitter/TweetsViewController.swift
+++ Twitter/TweetsViewController.swift
@@ -8,22 +8,64 @@
 
 import UIKit
 
-class TweetsViewController: UIViewController {
+class TweetsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     var tweets: [Tweet]?
+    
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        
         TwitterClient.sharedInstance.homeTimelineWithParams(nil) { (tweets, error) -> () in
             self.tweets = tweets
+            self.tableView.reloadData()
         }
         
+        self.tableView.reloadData()
         print("TWEETSSS: \(self.tweets)")
     }
     
     @IBAction func onLogout(sender: AnyObject) {
         User.currentUser?.logout()
         
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if tweets != nil {
+            return tweets!.count
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("TweetTableViewCell", forIndexPath: indexPath) as! TweetTableViewCell
+        
+        cell.tweet = self.tweets?[indexPath.row]
+        
+        return cell
+    }
+    
+    /*
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation */
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
     }
 }
